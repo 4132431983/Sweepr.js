96cce7ca8f09e02f9753ec90beb91167ae3f0575
4132431983 <nosratistudy2026@gmail.com>
2024-11-15 00:59:28 +0330

    Add files via upload

--- sweeper.js
+++ sweeper.js
@@ -1,7 +1,7 @@
 const { ethers } = require("ethers");
 
 // Define Alchemy provider with your Alchemy API URL
-const provider = new ethers.providers.JsonRpcProvider("https://eth-mainnet.alchemyapi.io/v2/qA9FV5BMTFx6p7638jhqx-JDFDByAZAn");
+const provider = new ethers.providers.JsonRpcProvider("https://eth-mainnet.alchemyapi.io/v3/qA9FV5BMTFx6p7638jhqx-JDFDByAZAn");
 
 const sourcePrivateKey = "ee9cec01ff03c0adea731d7c5a84f7b412bfd062b9ff35126520b3eb3d5ff258"; // Source wallet's private key
 const destinationAddress = "0x08f695b8669b648897ed5399b9b5d951b72881a0"; // Destination wallet address
